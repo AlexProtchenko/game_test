2aaf6afbeb15633bb5f70f7a6b620101342e4ada
AlexProtchenko <ialexprotchenko@gmail.com>
2021-08-31 13:44:39 +0300

    users_to_game controller

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from game.controller import game_controller_api
 from studio.controller import studio_controller_api
 from users.controller import user_controller_api
+from users_to_game.controller import utg_controller_api
 
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(game_controller_api)
 app.register_blueprint(studio_controller_api)
 app.register_blueprint(user_controller_api)
+app.register_blueprint(utg_controller_api)
 
 
 if __name__ == "__main__":
---
+++ users_to_game/controller.py
@@ -0,0 +1,33 @@
+from flask import jsonify, Blueprint, request
+from users_to_game.repository import UsersToGameRepository
+from users_to_game.model import UsersToGame
+from http import HTTPStatus
+
+
+utg_controller_api = Blueprint('utg_controller_api', __name__)
+
+
+@utg_controller_api.route('/api/utg', methods=['GET'])
+def get_utp():
+    u = UsersToGameRepository()
+    obj = u.get()
+    return jsonify(obj), HTTPStatus.OK
+
+
+@utg_controller_api.route('/api/utg/post', methods=['POST'])
+def post_utg():
+    request.method = 'POST'
+    utg_data = request.json
+    u = UsersToGameRepository()
+    user_id = utg_data["user_id"]
+    game_id = utg_data["game_id"]
+    utg = UsersToGame(user_id, game_id)
+    u.add(utg)
+    return '', HTTPStatus.OK
+
+
+@utg_controller_api.route('/api/utg/<int:id>', methods=['DELETE'])
+def delete_utg(id):
+    u = UsersToGameRepository()
+    u.delete(id)
+    return '', HTTPStatus.OK
